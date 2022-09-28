7aa009f837425cd1500ffe09e2737e1c3be93610
notweerdmonk <wrdmnk@gmail.com>
2022-09-28 22:25:19 +0530

    Add license to project files.

--- include/uart.h
+++ include/uart.h
@@ -1,8 +1,32 @@
+/*
+  MIT License
+  
+  Copyright (c) 2020 - 2022 notweerdmonk
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 /**
  * @file uart.h
- * @brief Function prototypes for software UART.
+ * @brief Function prototypes for AVR software UART.
  *
- * This file contains prototypes for software UART. Include
+ * This file contains prototypes for AVR software UART. Include
  * it when using the library archive.
  *
  * @author notweerdmonk
